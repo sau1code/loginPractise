a2949c19be2552a31af0f0fc102bb1354b7b7490
sau1code <krys4ab@gmail.com>
2022-08-09 10:37:31 +0800

    test1

--- app/src/main/java/com/example/loginpractise/MainActivity.java
+++ app/src/main/java/com/example/loginpractise/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.d("main", "abc");
+                // 123132
             }
         });
 
