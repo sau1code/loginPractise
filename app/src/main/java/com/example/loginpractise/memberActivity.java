b4ac444612475885b2b79cfb510fede1eb2632aa
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-08-10 21:25:34 +0800

    Add ActionBar to back to front page in memberActivity

--- app/src/main/java/com/example/loginpractise/memberActivity.java
+++ app/src/main/java/com/example/loginpractise/memberActivity.java
@@ -1,9 +1,12 @@
 package com.example.loginpractise;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class memberActivity extends AppCompatActivity {
@@ -15,10 +18,20 @@ public class memberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member);
 
+        ActionBar actBar = getSupportActionBar();
+        actBar.setDisplayHomeAsUpEnabled(true);
         // 以下 "可"隨意刪除
         setTitle("activity_member");
         Intent intent = getIntent();
         textViewMemberTempshow = (TextView)findViewById(R.id.textView_member_tempshow);
         textViewMemberTempshow.setText("使用者帳號 : " + intent.getStringExtra("name"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
