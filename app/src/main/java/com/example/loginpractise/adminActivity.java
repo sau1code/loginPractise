0ed1a6db966fff70b738145b4f7cde96c4b80f21
sau1code <krys4ab@gmail.com>
2022-08-11 16:32:07 +0800

    admin sql

--- app/src/main/java/com/example/loginpractise/adminActivity.java
+++ app/src/main/java/com/example/loginpractise/adminActivity.java
@@ -3,12 +3,18 @@ package com.example.loginpractise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 public class adminActivity extends AppCompatActivity {
 
     private TextView textViewAdminTempshow;
+    private mySQLiteContract.mySQLiteDbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,28 @@ public class adminActivity extends AppCompatActivity {
         Intent intent = getIntent();
         textViewAdminTempshow = (TextView)findViewById(R.id.textView_admin_tempshow);
         textViewAdminTempshow.setText("管理員帳號 : "+intent.getStringExtra("name"));
+
+        dbHelper = new mySQLiteContract.mySQLiteDbHelper(adminActivity.this);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String table = mySQLiteContract.mySQLiteEntry.TABLE_NAME;
+
+        ((Button)findViewById(R.id.button_1)).setOnClickListener(v -> {
+            Cursor cursor = db.rawQuery("select * from " + table + ";", null);
+            int cursorCount = cursor.getCount();
+
+            StringBuilder sb = new StringBuilder();
+            cursor.moveToFirst();
+            do{
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    sb.append(cursor.getString(i)+"\n");
+                }
+                sb.append("-----------------\n");
+            } while(cursor.moveToNext());
+            ((TextView)findViewById(R.id.textView_output)).setText(sb);
+            cursor.close();
+        });
+
 
     }
 }
