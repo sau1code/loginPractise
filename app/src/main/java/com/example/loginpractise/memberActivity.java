32088d4255038f3cabb7aabb440899771850de2a
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-08-12 22:37:34 +0800

    add button to delete user account in memberActivity.java.

--- app/src/main/java/com/example/loginpractise/memberActivity.java
+++ app/src/main/java/com/example/loginpractise/memberActivity.java
@@ -2,21 +2,25 @@ package com.example.loginpractise;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.Picture;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.loginpractise.mySQLiteContract;
+import java.util.HashMap;
+import java.util.Map;
 
 public class memberActivity extends AppCompatActivity {
 
@@ -24,6 +28,9 @@ public class memberActivity extends AppCompatActivity {
     private mySQLiteContract.mySQLiteDbHelper dbHelper;
     private EditText editTextPassword,editTextPassword2,editTextName,editTextBirth;
     private EditText editTextPhone,editTextAddress,editTextEmail;
+    private String userpwd;
+    private Button buttonDelete,buttonModify;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +73,7 @@ public class memberActivity extends AppCompatActivity {
 
 //      set database to writable mode and
 //      return SQLiteDatabase object "db" to execute SQL command
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db = dbHelper.getWritableDatabase();
 
 //      Note: define the name of table and column as attributes in  class mySQLiteContract.mySQLiteEntry
 //      get table name : "customers"
@@ -77,12 +84,66 @@ public class memberActivity extends AppCompatActivity {
         Cursor userInfor = db.rawQuery("select * from " + table + whereCondition + ";", null);
         int rowCount = userInfor.getCount();
         Log.d("main","rowCount="+rowCount);
+//      -------------------put user information from Cursor object to Map-------------------
+        Map<String, String> alluserInfor = new HashMap<>();
+        //    資料表名稱 : customers
+//    欄位中文名稱 欄位名稱    Cursor Index
+//    id           userid       0
+//    帳號 	       user         1
+//    密碼 	       password     2
+//    姓名 	       username     3
+//    生日 	       userbirth    4
+//    手機 	       cellphone    5
+//    Email 	   useremail    6
+//    住址 	       useraddress  7
+        if(userInfor.moveToFirst()){
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_PWD,userInfor.getString(2));
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_USERNAME,userInfor.getString(3));
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_BIRTH,userInfor.getString(4));
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_PHONE,userInfor.getString(5));
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_ADDRESS,userInfor.getString(7));
+            alluserInfor.put(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_EMAIL,userInfor.getString(6));
+        }
+        Log.d("main","Map size="+alluserInfor.size());
 //      close Cursor
         userInfor.close();
+//     -----------------------------------------------------------------------------------------------
+//      set text in EditText widgets for displaying user information to user
+        editTextPassword.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_PWD));
+        editTextPassword2.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_PWD));
+        editTextName.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_USERNAME));
+        editTextBirth.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_BIRTH));
+        editTextPhone.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_PHONE));
+        editTextAddress.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_ADDRESS));
+        editTextEmail.setText(alluserInfor.get(mySQLiteContract.mySQLiteEntry.COLUMN_NAME_EMAIL));
 
 
+        buttonDelete = (Button) findViewById(R.id.button_member_delete);
+        buttonModify = (Button) findViewById(R.id.button_member_modify);
+//      monitoring button (delete user account)
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(memberActivity.this);
+                builder.setTitle("刪除會員帳號");
+                builder.setMessage("請您確認是否需刪除本帳號?");
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-
+                    }
+                });
+                builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        db.execSQL("delete from "+table+" "+whereCondition+";");
+                        finish();
+                    }
+                });
+                builder.show();
+            }
+        });
 //      -----------auto create user id start with "A00" --------------
 //     get the number of total rows of records in table customers and return a integer variable
 //      1. SQLiteDatabase.rawQuery function can exacute SQL command and return output as Cursor object
@@ -101,7 +162,7 @@ public class memberActivity extends AppCompatActivity {
 //      5. SQLiteDatabase.execSQL function can exacute SQL commands which don't return data .
 //        5.1 The SQL command parameter in db.execSQL function : insert new user data to table "customers"
 //        db.execSQL("insert into " + table + " values ('" + userid + "','" + username + "','123456','apple','1986/3/28','0958499577','test@gmail.com','桃園市');");
-        db.close();
+
 
 
     }
@@ -118,6 +179,7 @@ public class memberActivity extends AppCompatActivity {
 //    stop
     @Override
     protected void onDestroy() {
+        db.close();
         dbHelper.close();
         super.onDestroy();
     }
